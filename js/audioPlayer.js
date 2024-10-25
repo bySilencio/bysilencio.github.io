b50628314a9bc92dc584ea71a8b4b65da46f3248
Elvis <elvisnunez@me.com>
2024-10-25 11:16:28 +0200

    Better faster wave loading

--- js/audioPlayer.js
+++ js/audioPlayer.js
@@ -1,21 +1,26 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
   const playButton = document.getElementById('play-button');
   const playIcon = document.getElementById('play-icon');
   let isPlaying = false;
+
+  // Fetch the pre-generated waveform data from JSON
+  const response = await fetch('/silencio-sample.json');
+  const peaks = await response.json();
 
   const wavesurfer = WaveSurfer.create({
     container: '#waveform',
     waveColor: '#fff',
     progressColor: '#FF699F',
     responsive: true,
-    backend: 'mediaelement',
+    backend: 'mediaelement',  // Allows for streaming playback
     barWidth: 4,
     barGap: 2,
     barRadius: 4,
     height: 55,
   });
 
-  wavesurfer.load('/silencio-sample.m4a');
+  // Load the audio file with the pre-generated peaks for faster rendering
+  wavesurfer.load('/silencio-sample.aac', peaks);
 
   playButton.addEventListener('click', () => {
     if (isPlaying) {
